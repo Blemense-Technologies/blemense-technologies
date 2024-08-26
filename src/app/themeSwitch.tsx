10d9d16736ee53dbe0c6bf3c03727dabff73dd53
pg147 <xyzprathamesh147@gmail.com>
2024-08-26 19:24:25 +0530

    Changes in Theme Provider and Theme Switch Buttons changed

--- src/app/themeSwitch.tsx
+++ src/app/themeSwitch.tsx
@@ -7,12 +7,12 @@ export default function ThemeSwitch() {
     const { setTheme, resolvedTheme } = useTheme();
 
     if (resolvedTheme == 'dark') return (
-        <div onClick={() => setTheme('light')} className='flex items-center h-fit w-fit rounded-full lg:p-3 border border-[#ebebeb] cursor-pointer'>
+        <div onClick={() => setTheme('light')} className='flex items-center h-fit w-fit rounded-full lg:p-3 border-[1.5px] dark:bg-[#1F1F1E] dark:border-[#4E4E4E] border-[#ebebeb] cursor-pointer'>
             <HiOutlineSun className='h-5 w-5 text-secondaryTheme' />   
         </div>
     )
     if (resolvedTheme == 'light') return (
-        <div onClick={() => setTheme('dark')} className='flex items-center h-fit w-fit rounded-full lg:p-3 border border-[#ebebeb] cursor-pointer'>
+        <div onClick={() => setTheme('dark')} className='flex items-center h-fit w-fit rounded-full lg:p-3 border-[1.5px] dark:bg-[#1F1F1E] dark:border-[#4E4E4E] border-[#ebebeb] cursor-pointer'>
             <HiOutlineMoon className='h-5 w-5 text-primaryTheme' />   
         </div>
     )
