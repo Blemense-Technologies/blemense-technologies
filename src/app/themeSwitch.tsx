4c17ee6607f7ee94f1f60cd906ec6b5a5f95bca3
pg147 <xyzprathamesh147@gmail.com>
2024-08-27 19:37:33 +0530

    Minor styling changes in ThemeSwitch Button

--- src/app/themeSwitch.tsx
+++ src/app/themeSwitch.tsx
@@ -7,13 +7,13 @@ export default function ThemeSwitch() {
     const { setTheme, resolvedTheme } = useTheme();
 
     if (resolvedTheme == 'dark') return (
-        <div onClick={() => setTheme('light')} className='flex items-center h-fit w-fit rounded-full lg:p-3 border-[1.5px] dark:bg-[#1F1F1E] dark:border-[#4E4E4E] border-[#ebebeb] cursor-pointer'>
-            <HiOutlineSun className='h-5 w-5 text-secondaryTheme' />   
+        <div onClick={() => setTheme('light')} className='h-fit w-fit p-2 rounded-full lg:p-3 border-[1.5px] dark:bg-[#1F1F1E] dark:border-[#4E4E4E] border-[#ebebeb] cursor-pointer'>
+            <HiOutlineSun className='h-4 w-4 text-secondaryTheme' />   
         </div>
     )
     if (resolvedTheme == 'light') return (
-        <div onClick={() => setTheme('dark')} className='flex items-center h-fit w-fit rounded-full lg:p-3 border-[1.5px] dark:bg-[#1F1F1E] dark:border-[#4E4E4E] border-[#ebebeb] cursor-pointer'>
-            <HiOutlineMoon className='h-5 w-5 text-primaryTheme' />   
+        <div onClick={() => setTheme('dark')} className='h-fit w-fit p-2 rounded-full lg:p-3 border-[1.5px] dark:bg-[#1F1F1E] dark:border-[#4E4E4E] border-[#ebebeb] cursor-pointer'>
+            <HiOutlineMoon className='h-4 w-4 text-primaryTheme' />   
         </div>
     )
 }
