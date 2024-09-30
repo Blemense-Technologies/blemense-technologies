5b355a895767ee902c8188aef06cfcc96a10ecf2
pg147 <xyzprathamesh147@gmail.com>
2024-09-30 23:46:07 +0530

    Old content from Hero Section removed and Added Code Snippet

---
+++ src/components/code-snippet.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import ReactLogo from "../../public/logo-react.svg";
+import JSLogo from "../../public/logo-js.svg";
+
+export default function CodeSnippet() {
+    return (
+        <div className="absolute bottom-0 h-fit w-fit px-3 py-4 rounded-xl bg-[#202020] border border-[#333333] shadow-[0_8px_35px_rgb(239,170,58,0.1)]">
+            <div className="flex w-fit space-x-3 items-center">
+                <div className="bg-[#2A2A2A] px-3 flex items-center w-fit h-fit p-2 space-x-3 rounded-[12px]">
+                    <Image
+                        src={ReactLogo}
+                        height={50}
+                        width={50}
+                        alt="react_js"
+                        className="h-4 w-4 text-[#04D0F7]"
+                    />
+                    <h1 className="text-white text-sm">app.tsx</h1>
+                </div>
+                <div className="bg-[#2A2A2A] px-3 flex items-center w-fit h-fit p-2 space-x-3 rounded-[12px]">
+                    <Image
+                        src={JSLogo}
+                        height={50}
+                        width={50}
+                        alt="js_logo"
+                        className="h-4 w-4 text-[#F7DF1E]"
+                    />
+                    <h1 className="text-white text-sm">index.js</h1>
+                </div>
+                <div className="flex items-center space-x-3 w-fit">
+                    <div className="rounded-full h-2.5 w-2.5 bg-[#F45550]"></div>
+                    <div className="rounded-full h-2.5 w-2.5 bg-[#F4B63D]"></div>
+                    <div className="rounded-full h-2.5 w-2.5 bg-[#30C044]"></div>
+                </div>
+            </div>
+            <div className="flex space-x-4 mt-4">
+                <div className="grid items-center space-y-2 bg-[#2a2a2a] h-fit w-fit p-2 rounded-lg">
+                    {
+                        Array.from({ length: 4 }).map((_, index) => (
+                            <div className="px-2 rounded-[12px]" key={index}>
+                                <span className="text-[#FFB041] font-medium">{index + 1}</span>
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className="w-full bg-[#2a2a2a] rounded-[12px]">
+                    <div className="grid items-center space-y-2 px-4 py-2 w-fit">
+                        <p><span className="text-[#7E6DFF]">const</span> <span className="text-[#04D0F7]">dev</span> <span className="text-[#FF9704]">=</span> <span className="text-[#7E6DFF]">{`() => {`}</span></p>
+                        <p className="text-white">{`<`}<span className="text-[#FF4F04]">h1</span>{`>`} Say hi ! {`</`}<span className="text-[#FF4F04]">h1</span>{`>`}</p>
+                        <p className="text-[#7E6DFF]">{`}`}</p>
+                        <p className="text-white"><span className="text-[#7E6DFF]">export</span> <span className="text-[#04D0F7]">dev</span>;</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
 import HeroBackground from '../../public/hero-bg.png'
+import CodeSnippet from './code-snippet'
 
 export default function Hero() {
   return (
-    <div className='h-screen lg:h-fit block w-full'>
-      <div className='absolute z-[20] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-        <h1 className='text-2xl text-center'>Building <span className='text-primaryTheme'>NextGen</span> Tech <span className='text-secondaryTheme'>Solutions</span></h1>
-        <p className='text-subtitle dark:text-subtitleDark'>We bring together creative minds and technical expertise to craft innovative solutions that empower your business.</p>
-      </div>
+    <div className='relative h-screen w-full'>
+      <CodeSnippet />
     </div>
   )
 }
