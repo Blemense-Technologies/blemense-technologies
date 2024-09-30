79f05a9bd64d061c4ca972c8897f63659501ef45
pg147 <xyzprathamesh147@gmail.com>
2024-09-30 21:41:58 +0530

    Navbar Responsiveness added

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,17 +1,56 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle } from './ui/navigation-menu'
-import { cn } from '@/lib/utils'
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from './ui/navigation-menu'
 import { Button } from './ui/button'
 import Logo from '../../public/Logo.png'
+import { HiBars3 } from 'react-icons/hi2'
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from './ui/sheet'
 
 const navLinkClass = `group text-white inline-flex h-10 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50`
 
 export default function Navbar() {
     return (
         <div>
-            <div className='flex py-6 items-center w-full lg:justify-around'>
+            {/* Mobile View */}
+            <div className='md:hidden lg:hidden px-4 mt-2'>
+                <div className='flex space-x-2 items-center'>
+                    {/* Mobile Menu */}
+                    <Sheet>
+                        <SheetTrigger asChild>
+                            <div className='p-1 h-fit w-fit'>
+                                <HiBars3 className='h-7 w-7 text-white' />
+                            </div>
+                        </SheetTrigger>
+                        <SheetContent side={'left'}>
+                            <SheetHeader>
+                                <SheetTitle>Edit profile</SheetTitle>
+                                <SheetDescription>
+                                    Make changes to your profile here. Click save when you're done.
+                                </SheetDescription>
+                            </SheetHeader>
+                            <div className="grid gap-4 py-4">
+
+                            </div>
+                            <SheetFooter>
+                                <SheetClose asChild>
+                                    <Button type="submit">Save changes</Button>
+                                </SheetClose>
+                            </SheetFooter>
+                        </SheetContent>
+                    </Sheet>
+                    <Image
+                        src={Logo}
+                        height={100}
+                        width={100}
+                        alt={"blemense.tech.logo"}
+                        className='h-14 w-14'
+                    />
+                </div>
+            </div>
+
+            {/* Web View */}
+            <div className='hidden md:flex lg:flex py-6 items-center w-2/3 mx-auto lg:justify-between'>
                 <div>
                     <Image
                         src={Logo}
@@ -22,7 +61,7 @@ export default function Navbar() {
                     />
                 </div>
                 <NavigationMenu>
-                    <NavigationMenuList>
+                    <NavigationMenuList className='lg:space-x-12'>
                         <NavigationMenuItem>
                             <Link href="/" legacyBehavior passHref>
                                 <NavigationMenuLink className={navLinkClass}>
