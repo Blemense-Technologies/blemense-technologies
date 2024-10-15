6b22fce0a1115e7a23d1df12c046dd132d2662d2
pg147 <xyzprathamesh147@gmail.com>
2024-10-15 23:17:19 +0530

    Social Handles data added in Constants

---
+++ src/constants/socials.tsx
@@ -0,0 +1,8 @@
+import { Facebook, Instagram, Linkedin, Twitter } from "./icons";
+
+export const socialHandles = [
+    { icon: Instagram, border: 'insta', label: "Instagram", href: "https://www.instagram.com" },
+    { icon: Linkedin, border: 'linkedin', label: "LinkedIn", href: "https://www.linkedin.com" },
+    { icon: Facebook, border: 'facebook', label: "Facebook", href: "https://www.facebook.com" },
+    { icon: Twitter, border: 'twitter', label: "X / Twitter", href: "https://www.twitter.com" }
+];
