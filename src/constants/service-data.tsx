0f2706bac1631a10f53f6550aa66f44a9f5fc98a
pg147 <xyzprathamesh147@gmail.com>
2024-10-16 20:21:29 +0530

    Service Data added in constants

---
+++ src/constants/service-data.tsx
@@ -0,0 +1,22 @@
+export const serviceData = [
+    {
+        title: "Software Development",
+        description: "Build the backbone of your digital future. We deliver top-tier software that drives growth and innovation.", 
+    },
+    {
+        title: "UI / UX Designing",
+        description: "Our team builds responsive, user-friendly websites that showcase your brand, engage your audience, and drive conversions."
+    },
+    {
+        title: "Digital Marketing",
+        description: "We leverage the latest techniques to drive traffic, generate leads, and boost your brand's reputation."
+    },
+    {
+        title: "Website Development",
+        description: "Our team specializes in building responsive, visually stunning websites that are optimized for speed, functionality, and user engagement."
+    },
+    {
+        title: "Database Management",
+        description: "We provide expert maintenance to safeguard your information and enable informed decision-making."
+    }
+];
