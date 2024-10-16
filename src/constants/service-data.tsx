75e61bc7082e428a2bcfd53a1552cd91fbedfaaf
pg147 <xyzprathamesh147@gmail.com>
2024-10-16 21:12:23 +0530

    Minor Changes in Service data objects

--- src/constants/service-data.tsx
+++ src/constants/service-data.tsx
@@ -1,22 +1,38 @@
-export const serviceData = [
+type Service = {
+    title: string;
+    description: string;
+    gradientClass: string;
+};
+
+export const serviceData: Service[] = [
     {
         title: "Software Development",
-        description: "Build the backbone of your digital future. We deliver top-tier software that drives growth and innovation.", 
+        description:
+            "Build the backbone of your digital future. We deliver top-tier software that drives growth and innovation.",
+        gradientClass: "bg-gradient-to-br from-[#000000] to-[#666666]",
     },
     {
         title: "UI / UX Designing",
-        description: "Our team builds responsive, user-friendly websites that showcase your brand, engage your audience, and drive conversions."
-    },
-    {
-        title: "Digital Marketing",
-        description: "We leverage the latest techniques to drive traffic, generate leads, and boost your brand's reputation."
+        description:
+            "Our team builds responsive, user-friendly websites that showcase your brand, engage your audience, and drive conversions.",
+        gradientClass: "bg-gradient-to-r from-[#292929] to-[#505050]",
     },
     {
         title: "Website Development",
-        description: "Our team specializes in building responsive, visually stunning websites that are optimized for speed, functionality, and user engagement."
+        description:
+            "Our team specializes in building visually stunning websites that are optimized for speed, functionality, and user engagement.",
+        gradientClass: "bg-gradient-to-r from-[#1E1E1E] to-[#444444]",
+    },
+    {
+        title: "Digital Marketing",
+        description:
+            "We leverage the latest techniques to drive traffic, generate leads, and boost your brand's reputation.",
+        gradientClass: "bg-gradient-to-r from-[#3A3A50] to-[#5A5A7A]",
     },
     {
         title: "Database Management",
-        description: "We provide expert maintenance to safeguard your information and enable informed decision-making."
-    }
+        description:
+            "We provide expert maintenance to safeguard your information and enable informed decision-making.",
+        gradientClass: "bg-gradient-to-r from-[#444430] to-[#7A7A50]",
+    },
 ];
