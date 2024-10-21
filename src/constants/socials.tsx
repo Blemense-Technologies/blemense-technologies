ce602ddb453f6ce599df0c66162c6ccc9fb0f785
Ojal Sharnagat <ojal.s.official@gmail.com>
2024-10-21 16:58:35 +0530

    social links added

--- src/constants/socials.tsx
+++ src/constants/socials.tsx
@@ -1,8 +1,8 @@
 import { Facebook, Instagram, Linkedin, Twitter } from "./icons";
 
 export const socialHandles = [
-    { icon: Instagram, border: 'insta', label: "Instagram", href: "https://www.instagram.com" },
-    { icon: Linkedin, border: 'linkedin', label: "LinkedIn", href: "https://www.linkedin.com" },
+    { icon: Instagram, border: 'insta', label: "Instagram", href: "https://www.instagram.com/blemense.tech/" },
+    { icon: Linkedin, border: 'linkedin', label: "LinkedIn", href: "https://www.linkedin.com/company/blemense-tech/" },
     { icon: Facebook, border: 'facebook', label: "Facebook", href: "https://www.facebook.com" },
-    { icon: Twitter, border: 'twitter', label: "X / Twitter", href: "https://www.twitter.com" }
+    { icon: Twitter, border: 'twitter', label: "X / Twitter", href: "https://x.com/BlemenseTech" }
 ];
